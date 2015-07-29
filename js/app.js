b4c8fa1dc213733cfaa62c5d11d35f7820f29ab8
Ben Cheng <benjamin.cheng90@gmail.com>
2015-07-29 12:03:29 -0400

    Add JS to make modal with overlay. Styled div

--- js/app.js
+++ js/app.js
@@ -8,5 +8,23 @@ $('document').ready(function(){
     return false;
   });
 
+  // Modal out the About
+  var $overlay = $('<div id="overlay"></div>');
+  var $image = $("#about-desc");
+  var $caption = $("<p></p>");
+
+  $overlay.append($image);
+  $("body").append($overlay);
+
+    //Capture the click event to show modal
+  $("#about-but").click(function(event){
+    $overlay.show();
+  });
+
+    //When overlay is clicked
+  $overlay.click(function(){
+      //Hide the overlay
+    $overlay.hide();
+  });
 });
 
